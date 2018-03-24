adba150970f21a46fb5b1f85a9653966fb79706e
Maciej Bartłomiej Kruk <m.b.kruk@gmail.com>
2018-03-24 16:37:01 +0100

    minor improvements

--- phasegradint.py
+++ phasegradint.py
@@ -8,23 +8,23 @@ def interp(phase,mask,x,y):
 	y = int(y+0.5)
 	#print("x, y =",x,y,flush=True)
 	ph = phase[y,x]
-	av = mask[y,x]
+	#av = mask[y,x]
 	av = 1.0
 	return av*np.cos(ph), av*np.sin(ph)
 
 @jit
-def integrate(phase,mask,x0,y0,r):
-	n = 100
-	da = 2.0*np.pi/n
-	v = 0.0
+def integrate(phase,mask,x0,y0,r,dl=0.25):
+
 	I = 0.0
 	for ix in range(-r,r+1):
 		for iy in range(-r,r+1):
 			if ix*ix+iy*iy<=r*r:
 				I += mask[y0+iy,x0+ix]
-	#if I>0:
-	#	print("I =",I,flush=True)
-	#I = mask[int(y0+0.5),int(x0+0.5)]
+
+
+	n = int(2.0*np.pi*r/dl+0.5)
+	da = 2.0*np.pi/n
+	v = 0.0
 	for i in range(n):
 		a = da*i
 		b = da*(i+1)
@@ -34,7 +34,8 @@ def integrate(phase,mask,x0,y0,r):
 		yb = y0+r*np.sin(b)
 		xa, ya = interp(phase,mask,xa,ya)
 		xb, yb = interp(phase,mask,xb,yb)
-		v += xa*yb-ya*xb
+		v += np.arcsin(xa*yb-ya*xb)
+
 	return I*v/da/r
 
 @jit
@@ -62,16 +63,15 @@ def select(mask_data,low,high,mask_threshold=0.02,mask_dilation=3):
 
 @jit
 def vorticityMap(phase,mask,r,mask_threshold=0.02,mask_dilation=3):
-	phase = np.array(phase)
-	mask_data = np.array(mask)
-
-	mask = cutoff(mask,mask_threshold,mask_dilation)
-
-	#print("mask:",np.min(mask),np.max(mask),flush=True)
 	assert(phase.shape==mask.shape)
+
+	phase = np.array(phase)
+	mask = cutoff(np.array(mask),mask_threshold,mask_dilation)
+
 	shape = phase.shape
 	v = np.zeros_like(mask)
 	for ix in range(r,shape[1]-r):
 		for iy in range(r,shape[0]-r):
 			v[iy,ix] = integrate(phase,mask,ix,iy,r)
+
 	return v
